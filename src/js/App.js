635e03aaabf1c2b6bd59456e3003e35973c8a2b0
Tony Kelly <Tonykelly1286@gmail.com>
2017-08-05 23:10:09 -0500

    init: First Commit

--- src/js/App.js
+++ src/js/App.js
@@ -1,20 +1,7 @@
 /**
  * Created by toned_000 on 3/5/2017.
  */
+
+
 const css = require('../Sass/main.scss');
-import React from 'react';
-import {render} from 'react-dom';
-import ContactList from './ContactList';
-class App extends React.Component{
-    render(){
-        return (
-            <div>
-                <h1>Contacts Lists</h1>
-                <ContactList />
-            </div>
 
-        )
-    }
-}
-
-render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,12 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin")
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : false,
-  entry: "./js/main.js",
+ 
+  entry: "./js/App.js",
   module: {
     rules: [
       {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']}
@@ -17,12 +18,17 @@ module.exports = {
     ]
   },
   output: {
-    path: __dirname + "/dist/",
-    filename: "main.min.js"
+    path: __dirname + "dist",
+    filename: "./main.min.js"
   },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
+  plugins: [
+    
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }), 
+    new HtmlWebpackPlugin({
+      title: 'Portfolio',
+      template: './index.html',
+    }),
+
   ],
 };
