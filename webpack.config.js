26306b1eb6f69c81c403a2cb962a9e90a7b43806
Tony Kelly <Tonykelly1286@gmail.com>
2017-08-05 19:00:03 -0500

    init: First Commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,27 +5,20 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
-
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
 module.exports = {
   context: path.join(__dirname, "src"),
   devtool: debug ? "inline-sourcemap" : false,
-  entry: "./js/client.js",
+  entry: "./js/main.js",
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
-        }
-      }
+    rules: [
+      {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']}
+    
     ]
   },
   output: {
-    path: __dirname + "/src/",
-    filename: "client.min.js"
+    path: __dirname + "/dist/",
+    filename: "main.min.js"
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
