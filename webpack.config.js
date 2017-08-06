b8cc81c91a8e58e793ed282a23a0b15ff53dbe1b
Tony Kelly <Tonykelly1286@gmail.com>
2017-08-06 15:45:37 -0500

    init: Loader, Plugins and Bootstrap3 added

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,51 @@
 /**
  * Created by toned_000 on 8/5/2017.
+ * var debug = process.env.NODE_ENV !== "production";
  */
+const glob = require('glob');
 
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const bootstrapEntryPoints = require('./webpack.bootstrap.config');
+const PurifyCSSPlugin = require('purifycss-webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var isProd = process.argv.indexOf('-p') !== -1; //will return true or false
+const cssDev = ['style-loader', 'css-loader', 'sass-loader'];
+const cssProd = ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use:[ 'css-loader', 'sass-loader'],
+          publicPath: '/dist'
+          
+        }) 
+        
+const cssConfig = isProd ? cssProd: cssDev;
+const bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
+
 module.exports = {
   context: path.join(__dirname, "src"),
  
-  entry: "./js/App.js",
+  entry:[bootstrapConfig,"./js/App.js"],
   module: {
     rules: [
       {
         test: /\.scss$/, 
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use:[ 'css-loader', 'sass-loader'],
-          
-        }) 
+        use: cssConfig,
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-      }
-    
+      },
+       {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: 'file-loader?name=[path][name].[ext]',
+      },
+      { test: /\.(woff2?|svg)$/, loader: 'url-loader?limit=10000&name=fonts/[name].[ext]' },
+      { test: /\.(ttf|eot)$/, loader: 'file-loader?name=fonts/[name].[ext]' },
+      //bootstrap 3
+      { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, 
+        loader: 'imports-loader?jQuery=jquery' },
     ]
   },
   output: {
@@ -49,6 +68,16 @@ module.exports = {
       template: './index.html',
       
     }),
-    new ExtractTextPlugin("app.css"),
+    new ExtractTextPlugin({
+      filename: '/css/[name].css',
+      disable: !isProd,
+      allChunks: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute! 
+      paths: glob.sync(path.join(__dirname, 'src/*.html')),
+    }),
   ],
 };
