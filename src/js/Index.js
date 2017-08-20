d3ecdcbaa160d3a39f17ae26b7029338155d5ec7
Tony Kelly <Tonykelly1286@gmail.com>
2017-08-19 22:48:13 -0500

    dev: added react router

--- src/js/Index.js
+++ src/js/Index.js
@@ -1,21 +1,26 @@
 import css from '../Sass/main.scss';
-
 import React from 'react';
 import ReactDom from 'react-dom';
 //creating redux store
 import {createStore} from 'redux';
 import allReducers from './reducers/reducer-index';
 const store = createStore(allReducers);
-
 import {Provider} from 'react-redux';
+//react router
+import {BrowserRouter, Route} from 'react-router-dom';
+
 import App from './App';
 
 //polyfill to make fetchAPI work in all browser
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 ReactDom.render(
+    <BrowserRouter>
+    <div>
 <Provider store = {store}>
-<App />
+<Route exact path='/' component={App} />
 </Provider>
+</div>
+</BrowserRouter>
 
 , document.getElementById('root'));
